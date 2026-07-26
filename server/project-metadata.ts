a9ae502c90c589345ede5b7f51bf6f01310fc3f6
tecxbro <tecxbro@users.noreply.github.com>
2026-07-25 18:49:00 -0700

    chore(project): establish Daniel project foundation

---
+++ server/project-metadata.ts
@@ -0,0 +1,11 @@
+import metadata from "../project-metadata.json";
+
+export const PROJECT_NAME = metadata.projectName;
+export const PACKAGE_NAME = metadata.packageName;
+export const PROJECT_VERSION = metadata.version;
+export const REPOSITORY_SLUG = metadata.repositorySlug;
+export const CANONICAL_REPOSITORY = metadata.canonicalRepository;
+export const UPGRADE_COMMAND = metadata.upgradeCommand;
+export const DEFAULT_CONFIG_DIRECTORY = metadata.defaultConfigDirectory;
+export const DEFAULT_USER_ID = metadata.defaultUserId;
+export const DEFAULT_BRANCH = metadata.defaultBranch;
